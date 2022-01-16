a084fa2886e1c9a63ba73ddaab9feb5408e5c399
Andy <a.kolkov@gmail.com>
2022-01-16 22:27:17 +0300

    test(package): Add test for remainder - 10. Recalculation with shift of coefficients is required.

--- est_test.go
+++ est_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // Test some valid numbers
 func TestValidNums(t *testing.T) {
-	validNums := []string{"639400", "666408", "166507", "310202", "165504", "0"}
+	validNums := []string{"639400", "666408", "166507", "310202", "165504", "0", "197511"}
 	for _, item := range validNums {
 		if err := Validate(item); err == ErrValueNotCommon {
 			t.Error("Valid number validated as invalid", item)
